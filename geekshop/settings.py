d758afbb710ac81b7bb7c2ad1918098e4a634b6f
Jahdie <jahdie@yandex.ru>
2021-06-27 14:24:31 +0700

    Task#6 context from json

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+# FIXTURE_DIRS = (os.path.join(PROJECT_ROOT, 'products\\fixtures'),)
