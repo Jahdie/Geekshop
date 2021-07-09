4afe3b542f38bcf0022c2a085d17f5d98c50485b
Jahdie <jahdie@yandex.ru>
2021-07-09 10:09:21 +0700

    add basket app, model

---
+++ basket/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from users.models import Users
+from products.models import Products
+
+
+class Baskets(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
